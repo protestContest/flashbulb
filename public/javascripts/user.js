84e36c9be4d0a2f77e726dd7ba122737eb83f29c
Zack Michener <zack@zackmichener.com>
2013-03-03 22:25:00 -0500

    add share dropdown

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -3,4 +3,21 @@ $(document).ready(function() {
         $(this).children('.thumbinfo').slideToggle("fast");
         $(this).find('.overlay-buttons').toggle();
     });
+
+    $(".icon-share").click(function() {
+        var img, imgTitle, imgPath;
+       
+       img = $(this).closest(".thumb").children("img");
+       imgPath = img.attr("src");
+       imgTitle = img.attr("alt");
+
+       $("#share-image").attr("src", imgPath);
+       $("#share-image-title").html(imgTitle);
+
+       $("#share.dropdown").slideDown();
+    });
+
+    $(".dropdown .close").click(function() {
+        $(this).closest(".dropdown").slideUp();
+    });
 });
