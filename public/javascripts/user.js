f51f06885cf9e542441f82bb4bcf0e9c35783e9e
Zack Michener <zack@zackmichener.com>
2013-03-07 15:18:59 -0500

    clean up extraneous code

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -8,16 +8,17 @@ $(document).ready(function() {
     $(".icon-share").click(function(evt) {
         that = $(this);
 
-        $("#share.dropdown").slideUp("fast", function() {
+        $("#share.dropdown").slideUp("3000", function() {
             var img, imgTitle, imgPath, imgRowTail, imgRowTailIndex, shareDropdown,
                 imgIndex, ircpcOffset;
 
             // remove dropdown
             shareDropdown = $(this).remove();
-            // image clicked
+
             img = that.closest(".thumb").children("img");
             imgPath = img.attr("src");
             imgTitle = img.attr("alt");
+
             // find the image at the beginning of this row
             imgIndex = $(".thumb").index(that.closest(".thumb"));
             imgRowTailIndex = Math.ceil((imgIndex + 1) / 4) * 4 - 1;
@@ -27,14 +28,21 @@ $(document).ready(function() {
 
             // put dropdown after row's last image
             shareDropdown.insertAfter(imgRowTail);
+
             // edit dropdown content
             $("#share-image").attr("src", imgPath);
             $("#share-image-title").html(imgTitle);
-            $("#share-link").attr("href", "/share" + imgPath);
+            imgPath = imgPath.replace(/ /g, "%20");
+            // TODO: make this point to public dropbox url
+            //$("#fbSource").attr("value", "@http://flashbulb.herokuapp.com" + imgPath);
+
             $("#irc_pc").css("left", ircpcOffset + "px");
+
             // show dropdown
             shareDropdown.slideDown("fast");
+
             // scroll window
+            console.log($(that).offset().top);
             if ($(that).offset().top + 490 > $(window).scrollTop() + $(window).height()) {
                 $("html, body").animate({
                     scrollTop: $(that).offset().top - ($(window).height() - 490)
@@ -48,20 +56,6 @@ $(document).ready(function() {
             });
 
 
-            $("#share-link").click(function() {
-                console.log("Sharing to " + "/share" + $("#share-image").attr("src"));
-                console.log(window.location.pathname);
-                $.post("/share" + $("#share-image").attr("src"), {
-                    "url": $("#share-image").attr("src"),
-                    "desc": $("#share-desc").val(),
-                    "returnURL": window.location.pathname
-                }, function(data, textStatus, jqXHR) {
-                    console.log(data);
-                    if (data.redirect) {
-                        window.location = data.redirect;
-                    }
-                });
-            });
         });
 
     });
@@ -69,6 +63,7 @@ $(document).ready(function() {
 
     $(window).scroll(function() {
         $(".toolbar.navbar-fixed-top").css("top",Math.max(0,52-$(this).scrollTop()));
-        console.log($(this).css("top"));
     });
+
 });
+
