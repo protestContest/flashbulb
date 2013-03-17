1b35732a5466d12fb1871ecf89bfecf6d3e3c733
Zack Michener <zack@zackmichener.com>
2013-03-16 23:49:20 -0400

    fix dropdown bug

--- public/script/src/Content.js
+++ public/script/src/Content.js
@@ -9,7 +9,7 @@ function Content (page, html) {
         });
 
         $(".icon-share").click(function(evt) {
-            $("#share.dropdown").slideUp("3000", showDropdown);
+            showDropdown($(this));
         });
     };
 
@@ -24,75 +24,80 @@ function Content (page, html) {
 }
 
 function showDropdown(that) {
-    var img, imgTitle, imgPath, imgRowTail, imgRowTailIndex, shareDropdown,
-        imgIndex, ircpcOffset;
 
-    // remove dropdown
-    shareDropdown = $(this).remove();
+    $("#share.dropdown").slideUp("fast", function() {
+        
+        var img, imgTitle, imgPath, imgRowTail, imgRowTailIndex, shareDropdown,
+            imgIndex, ircpcOffset;
 
-    img = that.closest(".thumb").children("img");
-    imgPath = img.attr("src");
-    imgTitle = img.attr("alt");
+        // remove dropdown
+        shareDropdown = $(this).remove();
 
-    // find the image at the beginning of this row
-    imgIndex = $(".thumb").index(that.closest(".thumb"));
-    imgRowTailIndex = Math.ceil((imgIndex + 1) / 4) * 4 - 1;
-    imgIndexInRow = imgIndex - (imgRowTailIndex - 3);
-    imgRowTail = that.closest(".row").children()[imgRowTailIndex];
-    ircpcOffset = 240 * imgIndexInRow + 100;
+        img = that.closest(".thumb").children("img");
+        imgPath = img.attr("src");
+        imgTitle = img.attr("alt");
 
-    // put dropdown after row's last image
-    shareDropdown.insertAfter(imgRowTail);
+        // find the image at the beginning of this row
+        imgIndex = $(".thumb").index(that.closest(".thumb"));
+        imgRowTailIndex = Math.ceil((imgIndex + 1) / 4) * 4 - 1;
+        imgIndexInRow = imgIndex - (imgRowTailIndex - 3);
+        imgRowTail = that.closest(".row").children()[imgRowTailIndex];
+        ircpcOffset = 240 * imgIndexInRow + 100;
 
-    // edit dropdown content
-    $("#share-image").attr("src", imgPath);
-    $("#share-image-title").html(imgTitle);
-    $("#fb-submit").html("Share on Facebook");
-    imgPath = imgPath.replace(/ /g, "%20");
+        // put dropdown after row's last image
+        shareDropdown.insertAfter(imgRowTail);
 
-    $("#irc_pc").css("left", ircpcOffset + "px");
+        // edit dropdown content
+        $("#share-image").attr("src", imgPath);
+        $("#share-image-title").html(imgTitle);
+        $("#fb-submit").html("Share on Facebook");
+        imgPath = imgPath.replace(/ /g, "%20");
 
-    // show dropdown
-    shareDropdown.slideDown("fast");
+        $("#irc_pc").css("left", ircpcOffset + "px");
 
-    // scroll window
-    if (img.offset().top + 490 > $(window).scrollTop() + $(window).height()) {
-        $("html, body").animate({
-            scrollTop: img.offset().top - ($(window).height() - 500)
-        });
-    }
+        // show dropdown
+        shareDropdown.slideDown("fast");
 
-    // register close button
-    $(".close").click(function() {
-        $(".dropdown").slideUp();
-    });
-
-    // facebook upload sumbit
-    $("#fb-submit").click(function(evt) {
-        evt.preventDefault();
-
-        $(this).html("<i class='icon-spinner icon-spin'></i> Sharing...");
-        $(this).attr("disabled", "disabled");
-
-        $.get("/public" + imgPath, function(publicUrl, status) {
-            var formData = new FormData();
-            formData.append("url", publicUrl.url);
-            formData.append("message", $("#share-desc").val());
-
-            $.ajax({
-                "url": $("#shareform").attr("action"),
-                "data": formData,
-                "cache": false,
-                "contentType": false,
-                "processData": false,
-                "type": "POST",
-                "success": function(data) {
-                    $("#fb-submit").html("Shared!");
-                }
+        // scroll window
+        if (img.offset().top + 490 > $(window).scrollTop() + $(window).height()) {
+            $("html, body").animate({
+                scrollTop: img.offset().top - ($(window).height() - 500)
             });
+        }
 
-            $("#fbSource").attr("value", "@" + publicUrl.url);
-        }, "json");
+        // register close button
+        $(".close").click(function() {
+            $(".dropdown").slideUp();
+        });
 
+        // facebook upload sumbit
+        $("#fb-submit").click(function(evt) {
+            evt.preventDefault();
+
+            $(this).html("<i class='icon-spinner icon-spin'></i> Sharing...");
+            $(this).attr("disabled", "disabled");
+
+            $.get("/public" + imgPath, function(publicUrl, status) {
+                var formData = new FormData();
+                formData.append("url", publicUrl.url);
+                formData.append("message", $("#share-desc").val());
+
+                $.ajax({
+                    "url": $("#shareform").attr("action"),
+                    "data": formData,
+                    "cache": false,
+                    "contentType": false,
+                    "processData": false,
+                    "type": "POST",
+                    "success": function(data) {
+                        $("#fb-submit").html("Shared!");
+                        setTimeout($("#share.dropdown").slideUp(), 1500);
+                    }
+                });
+
+                $("#fbSource").attr("value", "@" + publicUrl.url);
+            }, "json");
+
+        });
     });
 }
