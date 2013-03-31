fa4007ab7ba14dcd294f781ee086fa4797cb50cc
Zack Michener <zack@zackmichener.net>
2013-03-31 16:25:16 -0400

    add CRUD methods

--- models/Album.js
+++ models/Album.js
@@ -4,12 +4,64 @@ var mongoose = require("mongoose"),
     Photo = require("./Photo");
 
 var AlbumSchema = new Schema({
+    albumId: String,
     name: String,
     user: User,
     photos: [Photo]
 });
 
-// put methods here
+AlbumSchema.static("create", function(attrs, callback) {
+    if (attrs.user === undefined || attrs.user.email === undefined || 
+            attrs.name === undefined) {
+        callback("wrong attributes");
+    } else {
+        Album.findOne({
+            "albumId": attrs.user.email + ":" + attrs.name
+        }, function(err, album) {
+            if (err) { callback(err); }
+            else if (album) {
+                callback("album exists");
+            } else {
+                var newAlbum = new Album(attrs);
+                newAlbum.albumId = attrs.user.email + ":" + attrs.name;
+                newAlbum.save(function(err) {
+                    if (err) { callback(err); }
+                    else {
+                        callback(null, newAlbum);
+                    }
+                });
+            }
+        });
+    }
+});
+
+AlbumSchema.static("get", function(albumId, callback) {
+    Album.findOne({"albumId": albumId}, function(err, album) {
+        if (err) { callback(err); }
+        else if (album) {
+            callback(null, album);
+        } else {
+            callback("album not found");
+        }
+    });
+});
+
+AlbumSchema.method("update", function(attrs, callback) {
+    for (var attr in attrs) {
+        this[attr] = attrs[attr];
+    }
+
+    this.save(function(err) {
+        if (err) { callback(err); }
+        else {
+            callback(null);
+        }
+    });
+});
+
+AlbumSchema.method("destroy", function(callback) {
+    Album.remove({ email: this.email}, callback);
+});
 
 mongoose.model("Album", AlbumSchema);
 module.exports = mongoose.model("Album");
