9ecadfab464077ba2548f9e09054e5131c5a19ab
Zack Michener <zack@zackmichener.net>
2013-04-01 10:32:23 -0400

    begin creating Photo model

--- models/Photo.js
+++ models/Photo.js
@@ -4,12 +4,65 @@ var mongoose = require("mongoose"),
     Album = require("./Album");
 
 var PhotoSchema = new Schema({
-    user: User,
     album: Album,
     url: String
 });
 
-// put methods here
+PhotoSchema.static("create", function(attrs, callback) {
+    if (attrs.user === undefined || attrs.user.email === undefined || 
+            attrs.name === undefined) {
+        callback("wrong attributes");
+    } else {
+        Photo.findOne({
+            "albumId": attrs.user.email + ":" + attrs.name
+        }, function(err, album) {
+            if (err) { callback(err); }
+            else if (album) {
+                callback("album exists");
+            } else {
+                var newPhoto = new Photo(attrs);
+                newPhoto.albumId = attrs.user.email + ":" + attrs.name;
+                newPhoto.save(function(err) {
+                    if (err) { callback(err); }
+                    else {
+                        callback(null, newPhoto);
+                    }
+                });
+            }
+        });
+    }
+});
+
+PhotoSchema.static("get", function(albumId, callback) {
+    Photo.findOne({"albumId": albumId}, function(err, album) {
+        if (err) { callback(err); }
+        else if (album) {
+            callback(null, album);
+        } else {
+            callback("album not found");
+        }
+    });
+});
+
+PhotoSchema.method("update", function(attrs, callback) {
+    for (var attr in attrs) {
+        this[attr] = attrs[attr];
+    }
+
+    this.albumId = this.user.email + ":" + this.name;
+
+    this.save(function(err) {
+        if (err) { callback(err); }
+        else {
+            callback(null);
+        }
+    });
+});
+
+PhotoSchema.method("destroy", function(callback) {
+    Photo.remove({ albumId: this.albumId}, callback);
+});
 
 mongoose.model("Photo", PhotoSchema);
-module.exports = mongoose.model("Photo");
+var Photo = mongoose.model("Photo");
+module.exports = Photo;
