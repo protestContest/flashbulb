1540ace6b86116d4b4b146b351744796614a15fb
Zack Michener <zack@zackmichener.net>
2013-04-10 17:16:40 -0400

    change production redis provider

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.configure("development", function() {
 
 app.configure("production", function() {
     var store,
-        redisURL = url.parse(process.env.REDISTOGO_URL);
+        redisURL = url.parse(process.env.REDISCLOUD_URL);
     console.log("running in production environment");
     credentials = {
         "mongodb": {
