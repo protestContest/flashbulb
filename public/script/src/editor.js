f1efd9202c97a831b204ba08f3979aede079d5cc
Zack Michener <zack@zackmichener.net>
2013-04-13 12:30:20 -0400

    add mechanism for more filters

--- public/script/src/editor.js
+++ public/script/src/editor.js
@@ -2,12 +2,29 @@ $(document).ready(function() {
     var ctx = document.getElementById("editor").getContext("2d"),
         height = $("#editor img").height(),
         width = $("#editor img").width(),
-        img = new Image();
+        img = new Image(),
+        filters;
 
     img.src = "/photos" + $("#editor img").attr("src");
     img.onload = function() {
         width = img.width;
         height = img.height;
+        filters = createFilters();
+
+        // scale canvas to image size
+        $("#editor").attr("width", width);
+        $("#editor").attr("height", height);
+        ctx.drawImage(img, 0, 0);
+
+    };
+
+
+    $(".save").click(function(e) {
+        e.preventDefault();
+    });
+
+    function createFilters() {
+        var filters = {};
 
         // x-pro filter
         var gradWidth = width,
@@ -17,18 +34,13 @@ $(document).ready(function() {
         xpro.addColorStop(0.5, "rgba(0, 0, 0, 0.2");
         xpro.addColorStop(0.7, "rgba(0, 0, 0, 0.5");
         xpro.addColorStop(1, "rgba(0, 0, 0, 1)");
+        filters["xpro"] = xpro;
 
-        // scale canvas to image size
-        $("#editor").attr("width", width);
-        $("#editor").attr("height", height);
-        ctx.drawImage(img, 0, 0);
+        return filters;
+    }
 
-        ctx.fillStyle = xpro;
+    window.addFilter = function(filter) {
+        ctx.fillStyle = filters[filter];
         ctx.fillRect(0, 0, width, height);
-    };
-
-
-    $(".save").click(function(e) {
-        e.preventDefault();
-    });
+    }
 });
