a960f58834de02485d2d09f3d2312f7dd426f958
Zack Michener <zack@zackmichener.net>
2013-04-13 12:21:21 -0400

    add x-pro filter

--- public/script/src/editor.js
+++ public/script/src/editor.js
@@ -6,9 +6,25 @@ $(document).ready(function() {
 
     img.src = "/photos" + $("#editor img").attr("src");
     img.onload = function() {
-        $("#editor").attr("width", img.width);
-        $("#editor").attr("height", img.height);
+        width = img.width;
+        height = img.height;
+
+        // x-pro filter
+        var gradWidth = width,
+            gradHeight = height;
+        var xpro = ctx.createRadialGradient(gradWidth/2, gradHeight/2, 0, gradWidth/2, gradHeight/2, Math.min(gradWidth, gradHeight));
+        xpro.addColorStop(0, "rgba(0, 0, 0, 0)");
+        xpro.addColorStop(0.5, "rgba(0, 0, 0, 0.2");
+        xpro.addColorStop(0.7, "rgba(0, 0, 0, 0.5");
+        xpro.addColorStop(1, "rgba(0, 0, 0, 1)");
+
+        // scale canvas to image size
+        $("#editor").attr("width", width);
+        $("#editor").attr("height", height);
         ctx.drawImage(img, 0, 0);
+
+        ctx.fillStyle = xpro;
+        ctx.fillRect(0, 0, width, height);
     };
 
 
