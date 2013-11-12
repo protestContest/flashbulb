18de8045913f68e166bac2f5aece93b637892054
Zack Michener <zackm@accretivetg.com>
2013-11-12 08:45:46 -0800

    use abbreviations

--- routes.js
+++ routes.js
@@ -1,45 +1,47 @@
-function setup(app, handlers) {
-  app.get("/login/success", handlers.passport.authenticate("dropbox"),
-      handlers.application.login);
+function setup(app, h) {
+  app.get("/login/success", h.passport.authenticate("dropbox"),
+      h.app.login);
 
-  app.get("/auth/facebook", handlers.passport.authorize("facebook", {
+  app.get("/auth/facebook", h.passport.authorize("facebook", {
     scope: "publish_stream",
     failureRedirect: "/error"
   }));
-  app.get("/auth/facebook/success", handlers.passport.authorize("facebook", {
+  app.get("/auth/facebook/success", h.passport.authorize("facebook", {
     failureRedirect: "/error"
-  }), handlers.application.fbUpload);
-  app.get("/logout", handlers.application.logout);
+  }), h.app.fbUpload);
+  app.get("/logout", h.app.logout);
 
 
   // app
-  app.get("/", handlers.application.index);
-  app.get("/home", handlers.application.auth, handlers.application.home);
-  app.get("/error", handlers.application.error);
-  app.get("/login", handlers.passport.authenticate("dropbox"));
-  app.get("/all", handlers.application.auth, handlers.photo.all);
-  app.get("/help", handlers.application.help);
+  app.get("/", h.app.index);
+  app.get("/home", h.app.auth, h.app.home);
+  app.get("/error", h.app.error);
+  app.get("/login", h.passport.authenticate("dropbox"));
+  app.get("/all", h.app.auth, h.photo.all);
+  app.get("/help", h.app.help);
 
   // album
-  app.get("/albums", handlers.application.auth, handlers.album.all);
-  app.get("/albums/new", handlers.application.auth, handlers.album.createForm);
-  app.get("/albums/:album", handlers.application.auth, handlers.album.view);
-  app.post("/albums", handlers.application.auth, handlers.album.create);
-  app.delete("/albums/:album", handlers.application.auth, handlers.album.destroy);
+  app.get("/albums", h.app.auth, h.album.all);
+  app.get("/albums/new", h.app.auth, h.album.createForm);
+  app.get("/albums/:album", h.app.auth, h.album.view);
+  app.post("/albums", h.app.auth, h.album.create);
+  app.delete("/albums/:album", h.app.auth, h.album.destroy);
 
   // photo
-  app.get("/photos/all", handlers.application.auth, handlers.photo.all);
-  app.get("/photos/:album/:photo", handlers.application.auth, handlers.photo.get);
-  app.get("/photos/:photo", handlers.application.auth, handlers.photo.get);
-  app.post("/photos", handlers.application.auth, handlers.photo.upload);
-  app.post("/photos/:album", handlers.application.auth, handlers.photo.upload);
-  app.post("/move", handlers.application.auth, handlers.photo.move);
-  app.put("/photos/:album/:photo", handlers.photo.update);
-  app.put("/photos/:photo", handlers.photo.update);
+  app.get("/photos/all", h.app.auth, h.photo.all);
+  app.get("/photos/:album/:photo", h.app.auth, h.photo.get);
+  app.get("/photos/:photo", h.app.auth, h.photo.get);
+  app.post("/photos", h.app.auth, h.photo.upload);
+  app.post("/photos/:album", h.app.auth, h.photo.upload);
+  app.post("/move", h.app.auth, h.photo.move);
+  app.put("/photos/:album/:photo", h.photo.update);
+  app.put("/photos/:photo", h.photo.update);
 
-  app.get("/public/photos/:path", handlers.application.auth, handlers.photo.getPublicUrl);
-  app.get("/edit/:album/:photo", handlers.application.auth, handlers.photo.edit);
-  app.get("/edit/:photo", handlers.application.auth, handlers.photo.edit);
+  app.get("/public/photos/:path", h.app.auth, h.photo.getPublicUrl);
+  app.get("/edit/:album/:photo", h.app.auth, h.photo.edit);
+  app.get("/edit/:photo", h.app.auth, h.photo.edit);
+
+  app.get('/test/login', h.app.testLogin);
 }
 
 exports.setup = setup;
