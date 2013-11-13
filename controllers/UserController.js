5f8e921624865bfc959e030838c8d3852d8c1bd3
Zack Michener <zackm@accretivetg.com>
2013-11-12 16:18:51 -0800

    make better stubs

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -7,23 +7,23 @@ module.exports = function(creds) {
 // public API
 var UserController = {
   getTags: function(req, res) {
-
+    res.send(500);
   },
 
   getOne: function(req, res) {
-
+    res.send(500);
   },
 
   getAll: function(req, res) {
-
+    res.send(500);
   },
 
   createOrUpdate: function(req, res) {
-
+    res.send(500);
   },
 
   destroy: function(req, res) {
-
+    res.send(500);
   },
 
 };
