3ecda8658c6ec6e751cc1a7add4e5284b4cf86b6
Zack Michener <zackm@accretivetg.com>
2013-11-13 16:11:27 -0800

    add more routes tests

--- test/routes.js
+++ test/routes.js
@@ -3,6 +3,7 @@ var should = require('should')
   , request = require('supertest')
   , mongoose = require('mongoose')
   , app = require('../app')
+  , fixtures = require('../fixtures')
   ;
 
 describe('Routing', function() {
@@ -14,11 +15,17 @@ describe('Routing', function() {
     done();
   });
 
+  beforeEach(function(done) {
+    console.log(fixtures);
+    fixtures.run();
+    done();
+  });
+
   describe('User', function() {
     it('should create a non-existing user', function(done) {
       var body = {
         email: 'testUser1@flashbulb.com',
-        name: 'Test User',
+        name: 'New Test User',
         dropboxId: '234873567'
       };
 
@@ -36,6 +43,56 @@ describe('Routing', function() {
             res.body.dropboxId.should.equal(body.dropboxId);
             done();
           });
+    });
+
+    it('should update an existing user', function(done) {
+      var body = {
+        username: 'test1',
+        email: 'test1@example.com',
+        name: 'Different Name',
+        dropboxId: '00000000'
+      };
+
+      request(url)
+          .put('/api/users/test1')
+          .send(body)
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function(err, res) {
+            if (err) { throw err; }
+
+            res.body.should.have.property('_id');
+            res.body.email.should.equal(body.email);
+            res.body.name.should.equal(body.name);
+            res.body.dropboxId.should.equal(body.dropboxId);
+            done();
+          });
+    });
+
+    it('should remove an existing user', function(done) {
+      request(url)
+          .get('/api/users/test1')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function(err, res) {
+
+        if (err) { throw err; }
+        res.body.should.have.property('_id');
+        res.body.email.should.have.property(body.email);
+        res.body.name.should.have.property(body.name);
+        res.body.dropboxId.should.have.property(body.dropboxId);
+        request(url)
+            .delete('/api/users/test1')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(err, res) {
+
+          if (err) { throw err; }
+          res.body.should.not.have.property('_id');
+          done();
+
+        });
+      });
     });
   });
 
